789f2cc8aa043195cbceeb8fddb254de2ff21f25
kingdellaprogrammazione <edofru01@protonmail.com>
2024-12-12 22:59:39 +0100

    minor adjustements

--- source/QML8.py
+++ source/QML8.py
@@ -15,19 +15,20 @@ X_train, X_test, y_train, y_test, X_valid, y_valid = prepare_angle_data(input)
 # this automates data extraction, for angle
 
 now_encoding = ["angle"]
-now_num_layers = [5, 10]
+now_num_layers = [10,20,30]
 now_ansatz= ["Strong"]
+learning_rates = [1e-3, 5e-4, 1e-4]
 
 threshold =[ 0.48, 0.5 ,0.52]
 
-combinations = list(product(now_encoding, now_num_layers, now_ansatz))
+combinations = list(product(now_encoding, now_num_layers, now_ansatz, learning_rates))
 # Add counter to each combination
 lista_tot = [list(combo) + [idx + 1] for idx, combo in enumerate(combinations)]
 
 
 for i in lista_tot:
     model = VQC(num_wires=8, num_outputs=1, num_layers=i[1], encoding=i[0], reuploading=False) 
-    model = train(model, X_train, y_train,X_valid, y_valid, epochs=1, lr=0.001, verbose = False)
+    model = train(model, X_train, y_train,X_valid, y_valid, epochs=1000, lr=i[3], verbose = False)
     divider= '-'
     filename_weights='model' 
     filename_confusion = 'model' 
@@ -39,7 +40,7 @@ for i in lista_tot:
 
     filename_weights += '-weights.pth'
 
-    model_path_dict = current_file.parent.parent / 'models' / '8features' / filename_weights
+    model_path_dict = current_file.parent.parent / 'models' / '8features' / 'raw'/ filename_weights
     
     
     for h in  threshold:
@@ -55,8 +56,18 @@ for i in lista_tot:
         }   
         final_filename = filename_confusion + '-' + str(h) + '-report.pkl'
         
-        model_path_results = current_file.parent.parent / 'models' / '8features' / final_filename
+        model_path_results = current_file.parent.parent / 'models' / '8features' / 'raw' /  final_filename
 #        Save the results using joblib
         joblib.dump(evaluation_results, model_path_results)
+
+        readable_filename = filename_confusion +  '-' + str(h) + '-readablereport.txt'
+        model_path_results_readable = current_file.parent.parent / 'models' / '8features' / readable_filename
+        with open(model_path_results_readable, 'w+') as file:
+            file.write("Confusion Matrix:\n")
+            file.write(str(conf_matrix))  # Convert confusion matrix to string for saving
+
+            file.write("\n\nClassification Report:\n")
+            file.write(class_report)  # The classification report is already a string
+            file.close()
 
     model.save_model(model_path_dict)
